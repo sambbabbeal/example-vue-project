d34226a45fe0dda7196a4f62ec51369344aac5df
Samuel Bell-Bell <samuelbell-bell@macbook-pro-de-samuel.home>
2022-04-29 17:10:19 +0200

    First component example

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hello {{ arg }}</div>
+</template>
+<script>
+/*eslint-disable */
+
+export default {
+  name: "FirstComponent",
+  data() {
+    return {
+      arg: "hello",
+    };
+  },
+};
+/*eslint-disable */
+</script>
